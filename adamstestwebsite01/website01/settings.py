afa17c7c958eea18b940ad2d6c3ca54e543de6dd
OscorpNetwork01 <124637840+OscorpNetwork01@users.noreply.github.com>
2026-02-12 14:12:32 -0500

    Adding the structure for polls database and adding admin control for polls.

--- adamstestwebsite01/website01/settings.py
+++ adamstestwebsite01/website01/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',   #PollsConfig c'eset le fichier est en tantq ue tel
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
