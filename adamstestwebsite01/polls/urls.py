baad39b4ee9c67d8fe3ef0d7e66c5738b62ba2b6
OscorpNetwork01 <124637840+OscorpNetwork01@users.noreply.github.com>
2026-02-13 13:07:04 -0500

    Ajout du HTML et du backend de l'index des dernières questions posées.

--- adamstestwebsite01/polls/urls.py
+++ adamstestwebsite01/polls/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:question_id>/", views.detail, name="detail"),    #Va automatiquement activer la fonction views.detail pour display le bon ID
+    path("<int:question_id>/results/", views.results, name="results"), 
+    path("<question_id>/vote/", views.vote, name="vote"),
 ]
